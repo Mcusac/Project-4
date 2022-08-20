7737736d8192bfe1b877a976358a66a310ce7a84
Austin Lindsey <austin.lindsey@att.net>
2022-08-20 14:10:44 -0500

    8/20

---
+++ app.py
@@ -0,0 +1,115 @@
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+from sqlalchemy import inspect  
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///data.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+insp = inspect(engine) 
+tables = insp.get_table_names() 
+print (tables)
+
+new_table = Base.classes.new_table_name
+
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    print("test")
+    return("test")
+
+@app.route("/get_data")
+def names():
+    session = Session(engine)
+    
+    results = session.query(new_table.year, new_table.make, new_table.model, new_table.final_price, new_table.mileage, new_table.engine, new_table.zipcode).all()
+#  list of tuples, turning it a list of list to be jsonified
+    results = [list(r) for r in results]
+    
+    print(results)
+ 
+
+    return jsonify(results)
+
+    session.close()    
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
+# # # Save reference to the table
+# # Passenger = Base.classes.passenger
+
+# #################################################
+# # Flask Routes
+# #################################################
+
+# @app.route("/")
+# def welcome():
+#     """List all available api routes."""
+#     return (
+#         f"Available Routes:<br/>"
+#         f"/api/v1.0/names<br/>"
+#         f"/api/v1.0/passengers"
+#     )
+
+
+# @app.route("/api/v1.0/names")
+# def names():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
+
+#     """Return a list of all passenger names"""
+#     # Query all passengers
+#     results = session.query(Passenger.name).all()
+
+#     session.close()
+
+#     # Convert list of tuples into normal list
+#     all_names = list(np.ravel(results))
+
+#     return jsonify(all_names)
+
+
+# @app.route("/api/v1.0/passengers")
+# def passengers():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
+
+#     """Return a list of passenger data including the name, age, and sex of each passenger"""
+#     # Query all passengers
+#     results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+
+#     session.close()
+
+#     # Create a dictionary from the row data and append to a list of all_passengers
+#     all_passengers = []
+#     for name, age, sex in results:
+#         passenger_dict = {}
+#         passenger_dict["name"] = name
+#         passenger_dict["age"] = age
+#         passenger_dict["sex"] = sex
+#         all_passengers.append(passenger_dict)
+
+#     return jsonify(all_passengers)
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
