8ff56adcb8e853da215d9481ad97fdb9cc7cdab4
Mcusac <matthew.d.cusack@gmail.com>
2022-08-27 13:38:32 -0500

    fixes

--- app.py
+++ app.py
@@ -71,7 +71,6 @@ def data_table():
 
 @app.route("/search", methods = ["POST", 'GET'])
 def search():
-    check = 0
     
     try:
         year = float(request.form["inputYear"])
@@ -79,89 +78,89 @@ def search():
         mileage = float(request.form["inputMileage"])
 
         engine = float(request.form["inputEngine"])
-
-        make_BMW = 0
-        make_Chevrolet = 0
-        make_Ferrari = 0
-        make_Ford = 0
-        make_Honda = 0
-        make_Jaguar = 0
-        make_Jeep = 0
-        make_Land_Rover = 0
-        make_Mercedes_Benz = 0
-        make_Pontiac = 0
-        make_Porsche = 0
-        make_Toyota = 0
-        make_Volkswagen = 0
-        make_Other = 0
-
-        make = request.form["inputMake"]
-
-        if make == "BMW":
-            make_BMW = 1
-        elif make == "Chevrolet":
-            make_Chevrolet = 1
-        elif make == "Ferrari":
-            make_Ferrari = 1
-        elif make == "Ford":
-            make_Ford = 1
-        elif make == "Honda":
-            make_Honda = 1
-        elif make == "Jaguar":
-            make_Jaguar = 1
-        elif make == "Jeep":
-            make_Jeep = 1
-        elif make == "Land Rover":
-            make_Land_Rover = 1
-        elif make == "Mercedes-Benz":
-            make_Mercedes_Benz = 1
-        elif make == "Pontiac":
-            make_Pontiac = 1
-        elif make == "Ferrari":
-            make_Porsche = 1
-        elif make == "Toyota":
-            make_Toyota = 1
-        elif make == "Volkswagen":
-            make_Volkswagen = 1
-        elif make == "Other":
-            make_Other = 1
-
-        prediction = 0
-
-        X = [[year, 
-            mileage, 
-            engine,
-            make_BMW,
-            make_Chevrolet,
-            make_Ferrari,
-            make_Ford,
-            make_Honda,
-            make_Jaguar,
-            make_Jeep, 
-            make_Land_Rover,
-            make_Mercedes_Benz,
-            make_Pontiac,
-            make_Porsche,
-            make_Toyota,
-            make_Volkswagen,
-            make_Other]]
-
-        print(X)
-
-        filename = './Regressor_model.h5'
-        loaded_model = pickle.load(open(filename, 'rb'))
-
-        print(loaded_model.predict(X))
-
-        prediction = loaded_model.predict(X)
-
-        prediction = prediction[0]
-
-        prediction = round(prediction, 0)
-
-        return render_template("search.html", prediction = prediction)
     except:
         return render_template('search.html')
+    make_BMW = 0
+    make_Chevrolet = 0
+    make_Ferrari = 0
+    make_Ford = 0
+    make_Honda = 0
+    make_Jaguar = 0
+    make_Jeep = 0
+    make_Land_Rover = 0
+    make_Mercedes_Benz = 0
+    make_Pontiac = 0
+    make_Porsche = 0
+    make_Toyota = 0
+    make_Volkswagen = 0
+    make_Other = 0
+
+    make = request.form["inputMake"]
+
+    if make == "BMW":
+        make_BMW = 1
+    elif make == "Chevrolet":
+        make_Chevrolet = 1
+    elif make == "Ferrari":
+        make_Ferrari = 1
+    elif make == "Ford":
+        make_Ford = 1
+    elif make == "Honda":
+        make_Honda = 1
+    elif make == "Jaguar":
+        make_Jaguar = 1
+    elif make == "Jeep":
+        make_Jeep = 1
+    elif make == "Land Rover":
+        make_Land_Rover = 1
+    elif make == "Mercedes-Benz":
+        make_Mercedes_Benz = 1
+    elif make == "Pontiac":
+        make_Pontiac = 1
+    elif make == "Ferrari":
+        make_Porsche = 1
+    elif make == "Toyota":
+        make_Toyota = 1
+    elif make == "Volkswagen":
+        make_Volkswagen = 1
+    elif make == "Other":
+        make_Other = 1
+
+    prediction = 0
+
+    X = [[year, 
+        mileage, 
+        engine,
+        make_BMW,
+        make_Chevrolet,
+        make_Ferrari,
+        make_Ford,
+        make_Honda,
+        make_Jaguar,
+        make_Jeep, 
+        make_Land_Rover,
+        make_Mercedes_Benz,
+        make_Pontiac,
+        make_Porsche,
+        make_Toyota,
+        make_Volkswagen,
+        make_Other]]
+
+    print(X)
+
+    filename = '/models/Regressor_model.h5'
+    loaded_model = pickle.load(open(filename, 'rb'))
+
+    print(loaded_model.predict(X))
+
+    prediction = loaded_model.predict(X)
+
+    prediction = prediction[0]
+
+    prediction = round(prediction, 0)
+
+    return render_template("search.html", prediction = prediction)
+
 
 
     
